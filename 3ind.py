b458849ada93a7b033f9aa21e4098b759d212ed2
Thoqady <70960593+Thoqady@users.noreply.github.com>
2020-12-08 16:20:34 +0300

    Delete 3ind.py

--- 3ind.py
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env python3
-# -*- coding: utf-8 -*-
-if __name__ == '__main__':
-    s = str(input("Введите слово: "))
-    k = int(input("Введите номер буквы, которую хотите заменить "))
-    a = s.replace(s[k-1], s[0])
-    s1 = 1
-    print(a[s1:] + a[:s1])
